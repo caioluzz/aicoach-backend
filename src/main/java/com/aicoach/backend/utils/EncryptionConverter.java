b7190269e50402de9d5c114705933e6f4d62936a
Caio Luz <caiogustavo.luz@gmail.com>
2026-07-24 00:41:34 -0300

    feat: ajustando chave de criptografia no properties

--- src/main/java/com/aicoach/backend/utils/EncryptionConverter.java
+++ src/main/java/com/aicoach/backend/utils/EncryptionConverter.java
@@ -2,49 +2,50 @@ package com.aicoach.backend.utils;
 
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.util.Base64;
 
+@Component
 @Converter
 public class EncryptionConverter implements AttributeConverter<String, String> {
 
     private static final String ALGORITHM = "AES";
+    private static Key secretKey;
 
-    //TODO: mudar para uma chave secreta
-    private static final byte[] KEY = "ChaveSecreta1234".getBytes();
+    @Value("${garmin.security.encryption-key}")
+    public void setKey(String encryptionKey) {
+        byte[] decodedKey = Base64.getDecoder().decode(encryptionKey);
+        secretKey = new SecretKeySpec(decodedKey, ALGORITHM);
+    }
 
     @Override
     public String convertToDatabaseColumn(String attribute) {
-        if (attribute == null) {
-            return null;
-        }
+        if (attribute == null) return null;
         try {
-            Key key = new SecretKeySpec(KEY, ALGORITHM);
             Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, key);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             byte[] encryptedBytes = cipher.doFinal(attribute.getBytes());
             return Base64.getEncoder().encodeToString(encryptedBytes);
         } catch (Exception e) {
-            throw new RuntimeException("[EncryptionConverter] Erro ao criptografar o dado para o banco", e);
+            throw new RuntimeException("Erro ao criptografar o dado para o banco", e);
         }
     }
 
     @Override
     public String convertToEntityAttribute(String dbData) {
-        if (dbData == null) {
-            return null;
-        }
+        if (dbData == null) return null;
         try {
-            Key key = new SecretKeySpec(KEY, ALGORITHM);
             Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, key);
+            cipher.init(Cipher.DECRYPT_MODE, secretKey);
             byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(dbData));
             return new String(decryptedBytes);
         } catch (Exception e) {
-            throw new RuntimeException("[EncryptionConverter] Erro ao descriptografar o dado do banco", e);
+            throw new RuntimeException("Erro ao descriptografar o dado do banco", e);
         }
     }
 }
